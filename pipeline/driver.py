a45f38edd681bb96ae12bd157d7efcfe1d1a9a35
DKrakauer <DKrakauer@scu.edu>
2023-06-01 16:53:28 -0700

    the whole gang is here

--- pipeline/driver.py
+++ pipeline/driver.py
@@ -1,11 +1,22 @@
 from color import generate_color_palette
 from unity_gen import generate_base_unity_file_name, duplicate_unity_files, tweak_files
+from get_filenames import generate_categories, get_prefab_paths
 
 def generate_vfx(prompt):
     print("Recieved prompt:" + prompt)
     # Generate the color palette
     print("Generating color palette for prompt:" + prompt)
     color_palette = generate_color_palette(prompt)
+
+    # Generate category names, and file names, save them to a txt file.
+    categories = generate_categories()
+    base_effect_names = get_prefab_paths(categories)
+    print("Categories and file names generated:")
+    print(categories[0])
+    print(base_effect_names[0])
+
+    print("Ending early for testing purposes.")
+    return
 
     # Generate the base Unity file name
     print("Generating VFX starting point file name...")
@@ -14,9 +25,6 @@ def generate_vfx(prompt):
     if base_file_name is None:
         print("Failed to generate base Unity file name.")
         return
-
-    print("Ending early for testing purposes.")
-    return
 
     # Duplicate the Unity files
     duplicated_file_name = duplicate_unity_files(base_file_name)
--- pipeline/get_filenames.py
+++ pipeline/get_filenames.py
@@ -8,8 +8,63 @@ def print_file_names(folder_path):
             file_path = os.path.join(root, file_name)
             if file_path.endswith(".mat") or file_name.endswith('.prefab'):
                 file_name_with_subfolder = os.path.join(folder_name, file_name)
-                print(file_path + file_name_with_subfolder)
+                # print(file_path + file_name_with_subfolder)
 
 # Usage example
-folder_path = "UnityTechnologies\ParticlePack"
-print_file_names(folder_path)
+# folder_path = "assets"
+# print_file_names(folder_path)
+
+def get_subfolder_names(folder_path):
+    """
+    Get the subfolder names of a folder.
+
+    Args:
+        folder_path (str): The path of the folder.
+
+    Returns:
+        list: The subfolder names.
+    """
+    subfolder_names = []
+    for root, dirs, files in os.walk(folder_path):
+        for dir_name in dirs:
+            subfolder_names.append(dir_name)
+    return subfolder_names
+
+def generate_categories():
+    """
+    Generate the categories for the VFX.
+
+    Returns:
+        list: The categories.
+    """
+    return get_subfolder_names("assets")
+
+def get_prefab_paths(categories):
+    """
+    Get the prefab paths for the categories.
+
+    Args:
+        categories (list): The categories.
+
+    Returns:
+        list: The prefab paths.
+    """
+    prefab_paths = []
+    
+    for category in categories:
+        category_path = os.path.join("assets", category, "Prefabs")
+        prefab_list = []
+        
+        if os.path.exists(category_path):
+            for file_name in os.listdir(category_path):
+                if file_name.endswith(".prefab"):
+                    prefab_path = os.path.join(category_path, file_name)
+                    prefab_list.append(prefab_path)
+        
+        prefab_paths.append(prefab_list)
+    
+    return prefab_paths
+
+
+
+
